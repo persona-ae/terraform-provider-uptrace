df0ff43e0925f7df2eecaf9da8dc7a7e3bc91358
colm-persona <161365984+colm-persona@users.noreply.github.com>
2025-05-06 14:29:56 -0700

    fill out the uptrace client implementation more (#7)

--- internal/services/types.go
+++ internal/services/types.go
@@ -2,18 +2,42 @@ package uptrace
 
 // start response models
 
+// get
+
 type GetMonitorsResponse struct {
-	Count    int       `json:"count"`
-	Monitors []Monitor `json:"monitors"`
+	Count    int               `json:"count"`
+	Monitors []MonitorResponse `json:"monitors"`
 }
 
 type GetMonitorByIdResponse struct {
-	Monitor Monitor `json:"monitor"`
+	Monitor MonitorResponse `json:"monitor"`
+}
+
+type MonitorIdResponse struct {
+	Monitor monitorId `json:"monitor"`
+}
+
+type MonitorRequest struct {
+	monitorBase
+}
+
+type MonitorResponse struct {
+	monitorBase
+
+	ID             int            `json:"id"`
+	ProjectID      int            `json:"projectId"`
+	Status         string         `json:"status"`
+	UpdatedAt      float64        `json:"updatedAt"`
+	CheckedAt      int64          `json:"checkedAt"`
+	CreatedAt      float64        `json:"createdAt"`
+	Error          string         `json:"error"`
+	NextCheckTime  int64          `json:"nextCheckTime"`
+	RepeatInterval repeatInterval `json:"repeatInterval"`
 }
 
 // start response-model vocabulary
 
-type Monitor struct {
+type monitorBase struct {
 	// Monitor name.
 	Name string `json:"name"`
 	// Must be set to metric.
@@ -25,21 +49,21 @@ type Monitor struct {
 	// List of channel ids to send notifications.
 	ChannelIDs []int `json:"channelIds"`
 
-	Params         Params         `json:"params"`
-	CheckedAt      int64          `json:"checkedAt"`
-	CreatedAt      float64        `json:"createdAt"`
-	Error          string         `json:"error"`
-	ID             int            `json:"id"`
-	NextCheckTime  int64          `json:"nextCheckTime"`
-	ProjectID      int            `json:"projectId"`
-	RepeatInterval RepeatInterval `json:"repeatInterval"`
-	Status         string         `json:"status"`
-	UpdatedAt      float64        `json:"updatedAt"`
+	Params Params `json:"params"`
 }
 
 type Params struct {
 	Metrics []Metric `json:"metrics"`
 	Query   string   `json:"query"`
+
+	// optional fields below
+	Column           *string  `json:"column"`
+	MinAllowedValue  *float32 `json:"minAllowedValue"`
+	MaxAllowedValue  *float32 `json:"maxAllowedValue"`
+	GroupingInterval *float32 `json:"groupingInterval,omitempty"`
+	CheckNumPoint    *int     `json:"checkNumPoint,omitempty"`
+	NullsMode        *string  `json:"nullsMode,omitempty"`
+	TimeOffset       *float32 `json:"timeOffset,omitempty"`
 }
 
 type Metric struct {
@@ -47,6 +71,10 @@ type Metric struct {
 	Name  string `json:"name"`
 }
 
-type RepeatInterval struct {
+type repeatInterval struct {
 	Strategy string `json:"strategy"`
+}
+
+type monitorId struct {
+	Id string `json:"id"`
 }
--- internal/services/uptrace.go
+++ internal/services/uptrace.go
@@ -106,3 +106,69 @@ func (u *UptraceClient) GetMonitorById(ctx context.Context, id string) (*GetMoni
 
 	return &result, nil
 }
+
+func (u *UptraceClient) CreateMonitor(ctx context.Context, req MonitorRequest) (*MonitorIdResponse, error) {
+	endpoint := fmt.Sprintf("/internal/v1/projects/%s/monitors", u.ProjectID)
+
+	if req.Type != "metric" {
+		return nil, fmt.Errorf("error monitors must be of type \"metric\". You provided: %s", req.Type)
+	}
+
+	var result MonitorIdResponse
+	if err := u.do(ctx, "POST", endpoint, nil, &result); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
+
+func (u *UptraceClient) UpdateMonitor(ctx context.Context, id string, req MonitorRequest) (*MonitorIdResponse, error) {
+	endpoint := fmt.Sprintf("/internal/v1/projects/%s/monitors/%s", u.ProjectID, id)
+
+	var result MonitorIdResponse
+	if err := u.do(ctx, "PUT", endpoint, nil, &result); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
+
+func (u *UptraceClient) CreateErrorMonitor(ctx context.Context, req MonitorRequest) (*MonitorIdResponse, error) {
+	endpoint := fmt.Sprintf("/internal/v1/projects/%s/monitors", u.ProjectID)
+
+	if req.Type != "error" {
+		return nil, fmt.Errorf("error monitors must be of type \"error\". You provided: %s", req.Type)
+	}
+
+	var result MonitorIdResponse
+	if err := u.do(ctx, "POST", endpoint, nil, &result); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
+
+func (u *UptraceClient) UpdateErrorMonitor(ctx context.Context, id string, req MonitorRequest) (*MonitorIdResponse, error) {
+	endpoint := fmt.Sprintf("/internal/v1/projects/%s/monitors/%s", u.ProjectID, id)
+
+	if req.Type != "error" {
+		return nil, fmt.Errorf("error monitors must be of type \"error\". You provided: %s", req.Type)
+	}
+
+	var result MonitorIdResponse
+	if err := u.do(ctx, "PUT", endpoint, nil, &result); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
+
+func (u *UptraceClient) DeleteMonitor(ctx context.Context, id string) error {
+	endpoint := fmt.Sprintf("/internal/v1/projects/%s/monitors/%s", u.ProjectID, id)
+
+	var result any
+	if err := u.do(ctx, "DELETE", endpoint, nil, &result); err != nil {
+		return err
+	}
+	return nil
+}
